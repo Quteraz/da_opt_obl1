6ca6f5e3d422cca1bb48b3aca85c73199d52b5c6
Magnus Haukeboe <mhaukeboe@hotmail.com>
2018-10-06 22:59:41 +0200

    generating data

--- gen_data.py
+++ gen_data.py
@@ -1,1 +1,16 @@
 import numpy as np
+import csv
+
+data_100 = np.random.randint(1, 300, size=(100,100))
+data_1000 = np.random.randint(1, 300, size=(1000,1000))
+data_10000 = np.random.randint(1, 300, size=(10000,10000))
+
+with open('datasets/data_100', 'w') as csvfile:
+	writer = csv.writer(csvfile)
+	writer.writerows(data_100)
+with open('datasets/data_1000', 'w') as csvfile:
+	writer = csv.writer(csvfile)
+	writer.writerows(data_100)
+with open('datasets/data_10000', 'w') as csvfile:
+	writer = csv.writer(csvfile)
+	writer.writerows(data_100)
