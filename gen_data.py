6a08e2e54bf03ca514c40ee29d31e8aed963b3d5
Magnus Haukeboe <mhaukeboe@hotmail.com>
2018-10-14 00:01:30 +0200

    WIP

--- gen_data.py
+++ gen_data.py
@@ -16,15 +16,3 @@ with open('datasets/data_1000', 'w') as csvfile:
 with open('datasets/data_10000', 'w') as csvfile:
 	writer = csv.writer(csvfile)
 	writer.writerows(data_10000)
-
-# with open('datasets/path_100', 'w') as csvfile:
-# 	writer = csv.writer(csvfile)
-# 	writer.writerow(path_100)
-
-# with open('datasets/path_1000', 'w') as csvfile:
-# 	writer = csv.writer(csvfile)
-# 	writer.writerow(path_1000)
-
-# with open('datasets/path_10000', 'w') as csvfile:
-# 	writer = csv.writer(csvfile)
-# 	writer.writerow(path_1000)
