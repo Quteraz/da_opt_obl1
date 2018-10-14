77fc9b81e3fbfb8adb284fc4f27ff9618aa9e2fb
Magnus Haukeboe <mhaukeboe@hotmail.com>
2018-10-14 15:42:01 +0200

    broke everything

--- find_optimal_path.py
+++ find_optimal_path.py
@@ -1,10 +1,10 @@
 import numpy as np
 import csv
 
-with open('datasets/data_100', 'r') as csvfile:
+with open('datasets/data_1000', 'r') as csvfile:
 	reader = csv.reader(csvfile)
 	data = [list(reader)]
-with open('datasets/data_1000', 'r') as csvfile:
+with open('datasets/data_5000', 'r') as csvfile:
 	reader = csv.reader(csvfile)
 	data.append(list(reader))
 with open('datasets/data_10000', 'r') as csvfile:
@@ -12,9 +12,12 @@ with open('datasets/data_10000', 'r') as csvfile:
 	data.append(list(reader))
 
 def random_normal():
-	path = [np.random.permutation(100),np.random.permutation(1000),np.random.permutation(10000)]
-
+	# find random path
+	path = [np.random.permutation(1000),np.random.permutation(5000),np.random.permutation(10000)]
+	# Length in the format of [length of set 1000, length of set 5000, length of set 10000]
 	length = [0,0,0]
+
+	# iterete for each data set
 	for i in range(3):
 		last_pos = path[i][0]
 		for current_pos in path[i]:
@@ -23,21 +26,40 @@ def random_normal():
 			else:
 				length[i] += int(data[i][last_pos][current_pos])
 			last_pos = current_pos
-	return length
+
+	path_data = {
+		'short': {
+			'path':path[0],
+			'length':length[0]
+		},
+		'medium': {
+			'path':path[1],
+			'length':length[1]
+		},
+		'large': {
+			'path':path[2],
+			'length':length[2]
+		}
+	}
+	return path_data
 
 def random_iterative(ant):
-	length = random_normal()
+	# literally just freedy done ant number of times
+	path_data = random_normal()
+	# print(path_data)
 	for _ in range(ant):
-		new_length = random_normal()
-		for index, element in enumerate(new_length):
-			if element < length[index]:
-				length[index] = element
-	return length
+		new_data = random_normal()
+		for element in new_data:
+			print(new_data[element]['length'],path_data[element]['length'])
+			if new_data[element]['length'] < path_data[element]['length']:
+				print('Hey')
+				path_data[element] = element
+	return path_data
 
 def greedy():
-	pos = [np.random.randint(100),np.random.randint(1000),np.random.randint(10000)]
-
-	marked = [[0]*100,[0]*1000,[0]*10000]
+	pos = [np.random.randint(1000),np.random.randint(5000),np.random.randint(10000)]
+	start = pos
+	marked = [[0]*1000,[0]*5000,[0]*10000]
 	length = [0,0,0]
 
 	for i in range(3):
@@ -54,14 +76,15 @@ def greedy():
 					pass
 			length[i] += val
 			marked[i][pos[i]] = 1
+		length[i] += int(data[i][start[i]][pos[i]])
 	return length
 
 
-
-print('\n-------------------------------------------------------------------------------------')
-print ('Random\nLength: ',random_normal())
+print('\nGenerating paths')
+# print('-------------------------------------------------------------------------------------')
+# print ('Random\nLength: ',random_normal())
 print('-------------------------------------------------------------------------------------')
 print('Random Iterative\nLength:',random_iterative(1000))
 print('-------------------------------------------------------------------------------------')
-print('Greedy\ntest:',greedy())
+print('Greedy\nLength:',greedy())
 print('-------------------------------------------------------------------------------------\n')
--- gen_data.py
+++ gen_data.py
@@ -4,17 +4,17 @@ import os
 
 os.makedirs('datasets', exist_ok=True)
 
-data_100 = np.random.randint(1, 300, size=(100,100))
 data_1000 = np.random.randint(1, 300, size=(1000,1000))
+data_5000 = np.random.randint(1, 300, size=(5000,5000))
 data_10000 = np.random.randint(1, 300, size=(10000,10000))
-
-with open('datasets/data_100', 'w') as csvfile:
-	writer = csv.writer(csvfile)
-	writer.writerows(data_100)
 
 with open('datasets/data_1000', 'w') as csvfile:
 	writer = csv.writer(csvfile)
 	writer.writerows(data_1000)
+
+with open('datasets/data_5000', 'w') as csvfile:
+	writer = csv.writer(csvfile)
+	writer.writerows(data_5000)
 
 with open('datasets/data_10000', 'w') as csvfile:
 	writer = csv.writer(csvfile)
